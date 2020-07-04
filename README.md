fd9a11f87a42a14d299060750c68fef11ad4bf26
Joo Heredia <djjoo007uw@gmail.com>
2020-07-04 09:52:31 -0700

    add code to script.js

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # homework3
+
+Create an app that generates random password based on user-selected criteria
+Will run in the browser and feature dynamically updated HTML and CSS powered by JavaScript
+Feature responsive user interface ensuring it adapts to multiple screen size.
+
+Password Word Criteria:
+Confirm if user would like, lowercase, uppercase, special characters
+Function userInfo - define character length and ask user how many characters they want.
+
+Function with if for set parameters
+Set Math.random
--- script.js
+++ script.js
@@ -1,3 +1,59 @@
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var numbers = [0,1,2,3,4,5,6,7,8,9];
+var specialChar = ["@","%","+","'","!","#","$","?",":",".","(",")","{","}","[","]","~","`","_","-"];
+
+function userInfo () {
+  var length = parseInt(prompt("How Many Characters Would You Like in Your Password?"));
+  if (length < 8){  
+  alert ("Passwords Need to Be More Than 8 Characters");
+  return;
+} 
+  if (length > 129){
+    alert ("Passwords Need to be less than 128 Characters");
+    return;
+  }
+ var lowerCase = confirm ("Would you like lowercase Characters?");
+ var upperCase = confirm ("Would you like to use uppercase Characters?");
+ var specialChar = confirm ("Would you like Special Charaters?"); 
+ var numbers = confirm ("Would you like numbers?");
+ if (lowerCase === false && upperCase === false && specialChar === false && numbers === false) {
+   alert ("Please choose at least one type of character!!!!!!!");
+   return;
+ }
+ var userOptions = {
+   length: length,
+   lowerCase: lowerCase,
+   upperCase: upperCase,
+   specialChar: specialChar,
+   numbers: numbers,
+ };
+ return userOptions;
+}
+function generatePassword () {
+var criteria = userInfo();
+var results = [];
+var options = [];
+if (criteria.lowerCase === true) {
+  options = options.concat(lowerCase); 
+}
+if (criteria.upperCase === true) {
+  options = options.concat(upperCase);
+}
+if (criteria.specialChar === true) {
+  options = options.concat(specialChar);
+}
+if (criteria.numbers === true) {
+  options = options.concat(numbers);
+}
+if (criteria.length === true) {
+  options = options.concat(length);
+}
+for (var i = 0; i < criteria.length; i++) {
+  results.push(options[Math.floor(Math.random()*options.length)]);
+}
+return results.join("");
+}
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
